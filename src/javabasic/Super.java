7a1d885a5ae7ccab70861d371fcd187703d197f2
fantastzjy <849372293@qq.com>
2021-03-22 15:41:59 +0800

    advance

---
+++ src/javabasic/Super.java
@@ -0,0 +1,25 @@
+package javabasic;
+
+public class Super {
+    protected int number;
+
+    protected void showNumber() {
+        System.out.println("number = " + number);
+    }
+}
+
+class Sub extends Super {
+    public Sub(int i){
+        super.number = 10;
+        super.showNumber();
+    }
+    void bar() {
+        int i = 0;
+        super.number = 20;
+        super.showNumber();
+    }
+
+    public static void main(String[] args) {
+        new Sub(1).bar();
+    }
+}
