c239054db039a7e7fcc9e90984af0b2a97dc7489
fantastzjy <849372293@qq.com>
2021-06-04 08:15:49 +0800

    advance

--- src/juc/交替打印/ABC_Condition.java
+++ src/juc/交替打印/ABC_Condition.java
@@ -18,20 +18,21 @@ public class ABC_Condition {
         public void run() {
             try {
                 lock.lock();
-                System.out.println("a加锁");
+                System.err.println("a加锁");
                 for (int i = 0; i < 10; i++) {
                     while (count % 3 != 0) {//注意这里是不等于0，也就是说在count % 3为0之前，当前线程一直阻塞状态
-                        System.out.println("睡觉觉" + i);
+                        System.out.println("a睡觉觉" + i);
                         A.await(); // A释放lock锁   然后等待被唤醒
+                        System.out.println("a被唤醒");
                     }
-                    System.out.println("A");
+                    System.out.println("打印A");
                     count++;
                     B.signal(); // A执行完唤醒B线程
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
-                System.out.println("a释放锁");
+                System.err.println("a释放锁");
                 lock.unlock();
             }
         }
@@ -41,19 +42,21 @@ public class ABC_Condition {
         public void run() {
             try {
                 lock.lock();
-                System.out.println("b加锁");
+                System.err.println("b加锁");
                 for (int i = 0; i < 10; i++) {
                     while (count % 3 != 1) {
+                        System.out.println("b睡觉觉" + i);
                         B.await();// B释放lock锁，当前面A线程执行后会通过B.signal()唤醒该线程
+                        System.out.println("b被唤醒");
                     }
-                    System.out.println("B");
+                    System.out.println("打印B");
                     count++;
                     C.signal();// B执行完唤醒C线程
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
-                System.out.println("b释放锁");
+                System.err.println("b释放锁");
                 lock.unlock();
             }
         }
@@ -63,19 +66,21 @@ public class ABC_Condition {
         public void run() {
             try {
                 lock.lock();
-                System.out.println("c加锁");
+                System.err.println("c加锁");
                 for (int i = 0; i < 10; i++) {
                     while (count % 3 != 2) {
+                        System.out.println("c睡觉觉" + i);
                         C.await();// C释放lock锁，当前面B线程执行后会通过C.signal()唤醒该线程
+                        System.out.println("c被唤醒");
                     }
-                    System.out.println("C");
+                    System.out.println("打印C");
                     count++;
                     A.signal();// C执行完唤醒A线程
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
-                System.out.println("c释放锁");
+                System.err.println("c释放锁");
                 lock.unlock();
             }
         }
@@ -85,5 +90,8 @@ public class ABC_Condition {
         new ThreadA().start();
         new ThreadB().start();
         new ThreadC().start();
+
+
     }
+
 }
