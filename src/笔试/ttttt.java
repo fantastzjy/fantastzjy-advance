1cd3e6bf00028daa79eb9f05bafbd04837808102
fantastzjy <849372293@qq.com>
2021-11-06 21:23:37 +0800

    advance

---
+++ src/笔试/ttttt.java
@@ -0,0 +1,22 @@
+package 笔试;
+
+class sss {
+
+    static {
+        aaaaa = 20;
+        //System.out.println(aaaaa);
+    }
+
+    public static int aaaaa = 25;
+    //aaaaa = 30;
+
+
+}
+
+public class ttttt {
+
+    public static void main(String[] args) {
+        System.out.println(sss.aaaaa);
+    }
+
+}
