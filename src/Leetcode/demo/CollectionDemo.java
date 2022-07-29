504617b4ed539207d384a7db568aab10bb6e51c1
jiaying2.zhang <jiaying2.zhang@ly.com>
2022-07-29 10:49:03 +0800

    1

--- src/Leetcode/demo/CollectionDemo.java
+++ src/Leetcode/demo/CollectionDemo.java
@@ -1,8 +1,10 @@
 package Leetcode.demo;
 
+
 import com.alibaba.fastjson2.JSON;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @Package: Java原理相关.Java开发手册
@@ -12,9 +14,6 @@ import java.util.*;
  * @Description:
  */
 public class CollectionDemo {
-
-
-
 
     public static void main(String[] args) {
 
@@ -118,15 +117,45 @@ public class CollectionDemo {
         //}
 
 
-        //8
+        //8 【强制】 在 JDK7 版本及以上，Comparator 要满足如下三个条件，不然 Arrays.sort，Collections.sort 会报 IllegalArgumentException 异常。
+        //   说明：三个条件如下
+        //   1） x，y 的比较结果和 y，x 的比较结果相反。
+        //   2） x>y，y>z，则 x>z。
+        //   3） x=y，则 x，z 比较结果和 y，z 比较结果相同
 
 
-        //9
+        //反例：下例中没有处理相等的情况，实际使用中可能会出现异常：
+        //      new Comparator<Student>() {
+        //          @Override
+        //          public int compare(Student o1, Student o2) {
+        //              return o1.getId() > o2.getId() ? 1 : -1;
+        //          }
+        //      };
+
+
+
+
+        //9  集合初始化时，指定集合初始值大小。
+        //说明：HashMap 使用 HashMap(int initialCapacity) 初始化，
+        //正例：initialCapacity = (需要存储的元素个数 / 负载因子) + 1。注意负载因子（即 loaderfactor）默认为 0.75，如果暂时无法确定初始值大小，请设置为 16（即默认值）。
+        //反例：HashMap 需要放置 1024 个元素，由于没有设置容量初始大小，随着元素不断增加，容量 7 次被迫扩大，resize 需要重建 hash 表，严重影响性能。
+
 
         //10
+        //推荐】使用 entrySet 遍历 Map 类集合 KV，而不是 keySet 方式进行遍历。
+        //说明：
+        //     keySet 其实是遍历了 2 次，一次是转为 Iterator 对象，另一次是从 hashMap 中取出key 所对应的 value。
+        //     而 entrySet 只是遍历了一次就把 key 和 value 都放到了 entry 中，效率更高。如果是 JDK8，使用 Map.foreach 方法。
+
+        //正例：values()返回的是 V 值集合，是一个 list 集合对象；keySet()返回的是 K 值集合，是一个 Set 集合对象；entrySet()返回的是 K-V 值组合集合。
+
+
         HashMap<String, String> map = new HashMap<>();
         map.put("k1", "v1");
         map.put("k2", "v2");
+
+        //values()返回的是 V 值集合
+        map.values();
 
         //不推荐
         // 若获取 KV ，  keySet 其实是遍历了 2 次，一次是转为 Iterator 对象，另一次是从 hashMap 中取出key 所对应的 value。
@@ -149,12 +178,31 @@ public class CollectionDemo {
         });
 
 
+        //11. 【推荐】高度注意 Map 类集合 K/V 能不能存储 null 值的情况，如下表格：
+
+        //   集合类                Key              Value              Super              说明
+        //   Hashtable         不允许为 null     不允许为 null      Dictionary           线程安全
+        //   ConcurrentHashMap 不允许为 null     不允许为 null      AbstractMap      锁分段技术（JDK8:CAS）
+        //   TreeMap           不允许为 null       允许为 null      AbstractMap          线程不安全          ****
+        //   HashMap            允许为 null        允许为 null      AbstractMap          线程不安全
+
+        //反例： 由于 HashMap 的干扰，很多人认为 ConcurrentHashMap 是可以置入 null 值，而事实上，存储 null 值时会抛出 NPE 异常。 *****
+
+
+        //12. 【参考】合理利用好集合的有序性(sort)和稳定性(order)，避免集合的无序性(unsort)和不稳定性(unorder)带来的负面影响。
+        //     说明：有序性是指遍历的结果是按某种比较规则依次排列的。稳定性指集合每次遍历的元素次序是一定的。
+        //      如：ArrayList 是 order/unsort；HashMap 是 unorder/unsort；TreeSet 是order/sort。
+
+
+
+        //13. 【参考】利用 Set 元素唯一的特性，可以快速对一个集合进行去重操作，避免使用 List 的contains 方法进行遍历、对比、去重操作。
+
+
+
         System.out.println();
 
 
     }
-
-
 
 
 }
