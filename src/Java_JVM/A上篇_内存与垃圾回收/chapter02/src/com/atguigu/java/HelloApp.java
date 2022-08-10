c5df9d50dbf4ef4978ff07f6b0ba3d3f8e30e3a6
fantastzjy <849372293@qq.com>
2022-08-10 08:41:26 +0800

    更新

---
+++ src/Java_JVM/A上篇_内存与垃圾回收/chapter02/src/com/atguigu/java/HelloApp.java
@@ -0,0 +1,14 @@
+package Java_JVM.A上篇_内存与垃圾回收.chapter02.src.com.atguigu.java;
+
+/**
+ * @author shkstart
+ * @create 2020 上午 11:43
+ */
+public class HelloApp {
+    private static int a = 1;//prepare：a = 0 ---> initial : a = 1
+
+
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
+}
---
+++ src/Java_JVM/A上篇_内存与垃圾回收/chapter02/src/java/lang/ShkStart.java
@@ -0,0 +1,12 @@
+package Java_JVM.A上篇_内存与垃圾回收.chapter02.src.java.lang;
+
+/**
+ * @author shkstart
+ * @create 2020 下午 12:00
+ */
+public class ShkStart {
+
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
