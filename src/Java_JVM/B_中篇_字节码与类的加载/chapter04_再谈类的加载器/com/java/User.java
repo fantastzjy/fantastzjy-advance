0a12864e6b475a1b5d4276c66c7c7fe6e3204d26
fantastzjy <849372293@qq.com>
2022-08-10 08:42:23 +0800

    更新

---
+++ src/Java_JVM/B_中篇_字节码与类的加载/chapter04_再谈类的加载器/com/java/User.java
@@ -0,0 +1,16 @@
+package Java_JVM.B_中篇_字节码与类的加载.chapter04_再谈类的加载器.com.java;
+
+/**
+ * @author shkstart
+ * @create  0:23
+ */
+public class User {
+    private int id;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
---
+++ src/Java_JVM/B_中篇_字节码与类的加载/chapter04_再谈类的加载器/com/java1/Demo1.java
@@ -0,0 +1,12 @@
+package Java_JVM.B_中篇_字节码与类的加载.chapter04_再谈类的加载器.com.java1;
+
+/**
+ * @author shkstart
+ * @create 11:07
+ */
+public class Demo1 {
+    public void hot() {
+        System.out.println("OldDemo1---> NewDemo1");
+    }
+
+}
