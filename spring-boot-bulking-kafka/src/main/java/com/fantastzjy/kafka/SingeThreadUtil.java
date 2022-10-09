cc270f429871f4ae1101fdda91c4bf9b781e0a79
fantastzjy <849372293@qq.com>
2022-10-09 11:28:05 +0800

    更新

---
+++ spring-boot-bulking-kafka/src/main/java/com/fantastzjy/kafka/SingeThreadUtil.java
@@ -0,0 +1,26 @@
+package com.fantastzjy.kafka;
+
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+
+public class SingeThreadUtil {
+
+    private static ExecutorService kafkaConsumer;
+
+    public SingeThreadUtil() {
+    }
+
+    public static ExecutorService getKafkaConsumer() {
+        return kafkaConsumer;
+    }
+
+    static {
+        kafkaConsumer = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(),
+                new CustomizableThreadFactory("singe-Thread-pool-kfa"));
+    }
+}
---
+++ spring-boot-bulking-kafka/src/main/java/com/weiguanjishu/domain/model/User.java
@@ -0,0 +1,30 @@
+package com.weiguanjishu.domain.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    /**
+     * 名称
+     */
+    private String userName;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 地址
+     */
+    private String address;
+}
